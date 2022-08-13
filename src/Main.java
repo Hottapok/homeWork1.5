9eff2ae58951e2334864ed4273f41570fa702edf
hottapok <hottapok@gmail.com>
2022-08-13 20:16:12 +0400

    сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        double[] array2 = {1.57, 7.654, 9.986};
+        boolean[] array3 = {true, false};
+
+        //task2
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.println(array[i]);
+            } else {
+                System.out.print(array[i] + ",");
+            }
+        }
+        for (int i = 0; i < array2.length; i++) {
+            if (i == array2.length - 1) {
+                System.out.println(array2[i]);
+            } else {
+                System.out.print(array2[i] + ",");
+            }
+        }
+        for (int i = 0; i < array3.length; i++) {
+            if (i == array3.length - 1) {
+                System.out.println(array3[i]);
+            } else {
+                System.out.print(array3[i] + ",");
+            }
+        }
+        System.out.println();
+        //task3
+        for (int i = array.length-1;i>=0;i--){
+            if (i==0){
+                System.out.println(array[i]);
+            }else System.out.print(array[i]+",");
+        }
+        for (int i= array2.length-1;i>=0;i--){
+            if(i==0){
+                System.out.println(array2[i]);
+            }else System.out.print(array2[i]+",");
+        }
+        for (int i=array3.length-1;i>=0;i--){
+            if (i==0){
+                System.out.println(array3[i]);
+            }else System.out.print(array3[i]+",");
+        }
+        System.out.println();
+        //task4
+        for (int i=0;i< array.length;i++){
+            if (array[i]%2!=0){
+                System.out.print((array[i]+1)+" ");
+            } else System.out.printf(array[i]+" ");
+        }
     }
 }
